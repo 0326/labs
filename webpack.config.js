3f32736589473c053727339c9c4817e9f1e1ae27
0326 <winniringy@gmail.com>
2016-08-24 00:43:51 +0800

    init vue2.0

--- webpack.config.js
+++ webpack.config.js
@@ -1,57 +1,56 @@
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-
-var cssnext = require('postcss-cssnext');
-var nested = require('postcss-nested');
-var doiuse = require('doiuse');
-var wordwrap = require('wordwrap');
-
-var colors = require('colors');
-
-var postCSSConfig = function(webpack) {
-    return [
-        nested,
-        cssnext(),
-        doiuse({
-            onFeatureUsage: function(info) {
-                var source = info.usage.source;
-                // file is whole require path, joined with !'s.. we want the last part
-                var sourceFile = path.relative('.', source.input.file.split('!').pop())
-                var sourceLine = sourceFile + ':' + source.start.line;
-                // take out location info in message itself
-                var message = info.message.split(': ').slice(1).join(': ')
-                console.log('[doiuse]'.red + ' ' + sourceLine + ': ' + info.featureData.title + '\n');
-                console.log(wordwrap(4, process.stdout.columns - 1)(message) + '\n');
-            }
-        }),
-    ];
-};
+var path = require('path')
+var webpack = require('webpack')
 
 module.exports = {
-    entry: {
-        app: ['./src/js/app.js']
-    },
-    output: {
-        path: require('path').resolve('build'),
-        publicPath: '/',
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.css$/,
-                loader: ExtractTextPlugin.extract('css!postcss'),
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                loader: 'babel'
-            }
-        ]
-    },
-    postcss: postCSSConfig,
-    plugins: [
-      new ExtractTextPlugin('styles.css'),
-    ],
-};
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    publicPath: '/dist/',
+    filename: 'build.js'
+  },
+  resolveLoader: {
+    root: path.join(__dirname, 'node_modules'),
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.vue$/,
+        loader: 'vue'
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file',
+        query: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true
+  },
+  devtool: '#eval-source-map'
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.devtool = '#source-map'
+  // http://vue-loader.vuejs.org/en/workflow/production.html
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  ])
+}
